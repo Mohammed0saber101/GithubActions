9c570d7eb2b66898e5c100827523d33649bb7cb1
Mohammed0saber101 <Mohammed0saber101@gmail.com>
2026-02-28 13:24:47 +0300

    test: Add a shell script to compare Node.js application output against an expected string.

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,6 @@
 EXPECTED="Hello, World"
-ACTUAL=$(node src/app.js World!)
+# ACTUAL=$(node src/app.js World!)
+ACTUAL=$(node -e "console.log(require('./src/app')('World'))")
 
 if [ "$ACTUAL" = "$EXPECTED" ]; then
   echo "✅ Test passed"
